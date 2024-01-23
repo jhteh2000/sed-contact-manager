88f0cbed86427dddee568b3c32eb2454a32c1161
Jing Hong Teh <jing.teh23@imperial.ac.uk>
2024-01-23 16:52:02 +0000

    Added ContactInfo abstract class

---
+++ src/contacts/ContactInfo.java
@@ -0,0 +1,7 @@
+package contacts;
+
+public abstract class ContactInfo {
+    public abstract String contactInfo();
+    public abstract String contactInfoType();
+    public abstract void sendMessage(String msg);
+}
